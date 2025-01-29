0d552357f854d5f46ad775dc8205f5c0f605b3d4
mahabaleshwars <147705296+mahabaleshwars@users.noreply.github.com>
2025-01-29 17:20:52 +0530

    Update tools.sh

--- .github/workflows/tools.sh
+++ .github/workflows/tools.sh
@@ -3,5 +3,3 @@
 # Set the JAVA_VERSION variable
 JAVA_VERSION='21'
 
-# Export the JAVA_VERSION as an environment variable for use in GitHub Actions
-# echo "JAVA_VERSION=$JAVA_VERSION" >> $GITHUB_ENV
